6fd04eeae3665c58893f61fec5e48d56a6291cf1
G M Saiful Alam <59245133+SaifulAlam93@users.noreply.github.com>
2023-02-17 20:03:11 -0800

    angular JWT project added

---
+++ angular-15-jwt-auth/src/app/app.component.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { StorageService } from './_services/storage.service';
+import { AuthService } from './_services/auth.service';
+import { EventBusService } from './_shared/event-bus.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  private roles: string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username?: string;
+
+  eventBusSub?: Subscription;
+
+  constructor(
+    private storageService: StorageService,
+    private authService: AuthService,
+    private eventBusService: EventBusService
+  ) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.storageService.isLoggedIn();
+
+    if (this.isLoggedIn) {
+      const user = this.storageService.getUser();
+      console.log("USer---",user)
+      this.roles = user.user.role;
+
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
+      this.username = user.user.username;
+    }
+
+    this.eventBusSub = this.eventBusService.on('logout', () => {
+      this.logout();
+    });
+  }
+
+  logout(): void {
+    // this.authService.logout().subscribe({
+    //   next: res => {
+    //     console.log(res);
+    //     this.storageService.clean();
+
+    //     window.location.reload();
+    //   },
+    //   error: err => {
+    //     console.log(err);
+    //   }
+    // });
+
+    this.storageService.clean();
+
+    window.location.reload();
+  }
+}
